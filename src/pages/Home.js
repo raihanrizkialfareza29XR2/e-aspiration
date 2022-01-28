cfe5e987cab098e860dc5ce460bf30006fc8ac2b
Naufal Akbar <kbrnugroho@gmail.com>
2022-01-28 07:46:35 +0700

    steps new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,56 +6,67 @@ import SEO from '../components/SEO';
 function Home() {
     const RenderSteps = () => {
         return (
-            <div className="px-20">
-                <h1 className="mb-4 text-center text-lg font-medium text-gray-700">Bagaimana Aspirasi diproses?</h1>
-                <div className="flex">
-                    <div className="w-1/3 text-center px-6">
-                    <div className="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-                        <div className="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.995 24h-1.995c0-3.104.119-3.55-1.761-3.986-2.877-.664-5.594-1.291-6.584-3.458-.361-.791-.601-2.095.31-3.814 2.042-3.857 2.554-7.165 1.403-9.076-1.341-2.229-5.413-2.241-6.766.034-1.154 1.937-.635 5.227 1.424 9.025.93 1.712.697 3.02.338 3.815-.982 2.178-3.675 2.799-6.525 3.456-1.964.454-1.839.87-1.839 4.004h-1.995l-.005-1.241c0-2.52.199-3.975 3.178-4.663 3.365-.777 6.688-1.473 5.09-4.418-4.733-8.729-1.35-13.678 3.732-13.678 4.983 0 8.451 4.766 3.732 13.678-1.551 2.928 1.65 3.624 5.09 4.418 2.979.688 3.178 2.143 3.178 4.663l-.005 1.241zm-13.478-6l.91 2h1.164l.92-2h-2.994zm2.995 6l-.704-3h-1.615l-.704 3h3.023z"/></svg>
+            <section className="dark:bg-coolGray-800 dark:text-coolGray-100">
+                <div className="container mx-auto px-20 flex flex-col p-6">
+                    <h2 className="py-4 text-3xl font-semibold text-center">Bagaimana Asripasi Diproses?</h2>
+                    <div className="divide-y divide-coolGray-700">
+                        <div className="grid justify-center grid-cols-4 p-8 mx-auto space-y-8 lg:space-y-0">
+                            <div className="flex items-center justify-center lg:col-span-1 col-span-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-16 h-16">
+                                    <path d="M472,16H168a24,24,0,0,0-24,24V344a24,24,0,0,0,24,24H472a24,24,0,0,0,24-24V40A24,24,0,0,0,472,16Zm-8,320H176V48H464Z"></path>
+                                    <path d="M112,400V80H80V408a24,24,0,0,0,24,24H432V400Z"></path>
+                                    <path d="M48,464V144H16V472a24,24,0,0,0,24,24H368V464Z"></path>
+                                </svg>
+                            </div>
+                            <div className="flex flex-col justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
+                                <span className="text-xs tracking-wider uppercase dark:text-violet-400">Step 1</span>
+                                <span className="text-xl font-bold md:text-2xl">Veritatis dolores</span>
+                                <span className="mt-4 dark:text-coolGray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis, voluptates error alias dolorem praesentium sit soluta iure incidunt labore explicabo eaque, quia architecto veritatis dolores, enim cons equatur nihil ipsum.</span>
+                            </div>
                         </div>
-                        <div className="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-                        <h2 className="font-bold text-sm">Personal Info</h2>
-                        <p className="text-xs text-gray-600">
-                            Just your personal information
-                        </p>
+                        <div className="grid justify-center grid-cols-4 p-8 mx-auto space-y-8 lg:space-y-0">
+                            <div className="flex items-center justify-center lg:col-span-1 col-span-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-16 h-16">
+                                    <path d="M285.177,179l15.513-3.914-7.827-31.028-15.514,3.913a176.937,176.937,0,0,0-129.3,133.557l-3.407,15.633,31.266,6.814,3.406-15.634A145.559,145.559,0,0,1,285.177,179Z"></path>
+                                    <path d="M363.624,147.871C343.733,72.077,274.643,16,192.7,16,95.266,16,16,95.266,16,192.7c0,82.617,57,152.163,133.735,171.4A176.769,176.769,0,0,0,320.7,496c97.431,0,176.7-79.266,176.7-176.695C497.392,238.071,441.64,167.336,363.624,147.871ZM48,192.7C48,112.91,112.91,48,192.7,48s144.7,64.91,144.7,144.7-64.911,144.7-144.7,144.7S48,272.481,48,192.7ZM320.7,464c-60.931,0-115.21-38.854-135.843-94.792,2.6.115,5.214.184,7.843.184a176.862,176.862,0,0,0,32.7-3.047l97.625,97.625C322.247,463.983,321.473,464,320.7,464Zm41.528-6.083L260.26,355.954a176.9,176.9,0,0,0,43.662-26.072L408.37,434.33A144.385,144.385,0,0,1,362.223,457.917Zm69.3-45.692L326.851,307.557a177.082,177.082,0,0,0,27.911-44.5L457.67,365.964A144.661,144.661,0,0,1,431.519,412.225Zm33.594-84.073-99.42-99.42a176.785,176.785,0,0,0,3.7-36.036c0-3.285-.1-6.547-.276-9.787a145.054,145.054,0,0,1,96.276,136.4C465.392,322.276,465.291,325.224,465.113,328.152Z"></path>
+                                </svg>
+                            </div>
+                            <div className="flex flex-col justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
+                                <span className="text-xs tracking-wider uppercase dark:text-violet-400">Step 2</span>
+                                <span className="text-xl font-bold md:text-2xl">Iure incidunt labore</span>
+                                <span className="mt-4 dark:text-coolGray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis, voluptates error alias dolorem praesentium sit soluta iure incidunt labore explicabo eaque, quia architecto veritatis dolores, enim cons equatur nihil ipsum.</span>
+                            </div>
                         </div>
-                    </div>
-                    </div>
-                    <div className="flex-1 flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14 2h-7.229l7.014 7h-13.785v6h13.785l-7.014 7h7.229l10-10z"/></svg>
-                    </div>
-                    <div className="w-1/3 text-center px-6">
-                    <div className="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-                        <div className="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
-                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 22h-24v-20h24v20zm-1-19h-22v18h22v-18zm-4 13v1h-4v-1h4zm-6.002 1h-10.997l-.001-.914c-.004-1.05-.007-2.136 1.711-2.533.789-.182 1.753-.404 1.892-.709.048-.108-.04-.301-.098-.407-1.103-2.036-1.305-3.838-.567-5.078.514-.863 1.448-1.359 2.562-1.359 1.105 0 2.033.488 2.545 1.339.737 1.224.542 3.033-.548 5.095-.057.106-.144.301-.095.41.14.305 1.118.531 1.83.696 1.779.41 1.773 1.503 1.767 2.56l-.001.9zm-9.998-1h8.999c.003-1.014-.055-1.27-.936-1.473-1.171-.27-2.226-.514-2.57-1.267-.174-.381-.134-.816.119-1.294.921-1.739 1.125-3.199.576-4.111-.332-.551-.931-.855-1.688-.855-.764 0-1.369.31-1.703.871-.542.91-.328 2.401.587 4.09.259.476.303.912.13 1.295-.342.757-1.387.997-2.493 1.252-.966.222-1.022.478-1.021 1.492zm18-3v1h-6v-1h6zm0-3v1h-6v-1h6zm0-3v1h-6v-1h6z"/></svg>
+                        <div className="grid justify-center grid-cols-4 p-8 mx-auto space-y-8 lg:space-y-0">
+                            <div className="flex items-center justify-center lg:col-span-1 col-span-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-16 h-16">
+                                    <path d="M412.284,294.37l-12.5,15.642c-8.354,10.454-50.027,64.208-50.027,95.883,0,36.451,28.049,66.105,62.526,66.105s62.527-29.654,62.527-66.105c0-31.675-41.673-85.429-50.028-95.883Zm0,145.63c-16.832,0-30.526-15.3-30.526-34.105,0-11.662,15.485-37.883,30.531-59.2,15.043,21.3,30.522,47.509,30.522,59.2C442.811,424.7,429.116,440,412.284,440Z"></path>
+                                    <path d="M122.669,51.492,96.133,124.4,30.092,97.205,17.908,126.8l67.271,27.7L26.9,314.606a48.056,48.056,0,0,0,28.689,61.523l184.719,67.232a48,48,0,0,0,61.523-28.688L397.6,151.56Zm149.1,352.236a16,16,0,0,1-20.508,9.563L66.537,346.059a16,16,0,0,1-9.563-20.507L73.553,280H316.8ZM85.2,248l29.594-81.311,36.333,14.961a32.644,32.644,0,1,0,11.236-29.98l-36.615-15.077,16.046-44.085,214.79,78.177L328,249.219V248Z"></path>
+                                </svg>
+                            </div>
+                            <div className="flex flex-col justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
+                                <span className="text-xs tracking-wider uppercase dark:text-violet-400">Step 3</span>
+                                <span className="text-xl font-bold md:text-2xl">Dolorem praesentium</span>
+                                <span className="mt-4 dark:bg-coolGray-800 dark:text-coolGray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis, voluptates error alias dolorem praesentium sit soluta iure incidunt labore explicabo eaque, quia architecto veritatis dolores, enim cons equatur nihil ipsum.</span>
+                            </div>
                         </div>
-                        <div className="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-                        <h2 className="font-bold text-sm">Account Info</h2>
-                        <p className="text-xs text-gray-600">
-                            Anything you want for your credentials
-                        </p>
+                        <div className="grid justify-center grid-cols-4 p-8 mx-auto space-y-8 lg:space-y-0">
+                            <div className="flex items-center justify-center lg:col-span-1 col-span-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-16 h-16">
+                                    <polygon points="388.632 393.82 495.823 255.94 388.684 118.178 363.424 137.822 455.288 255.944 363.368 374.18 388.632 393.82"></polygon>
+                                    <polygon points="148.579 374.181 56.712 255.999 148.629 137.823 123.371 118.177 16.177 255.993 123.314 393.819 148.579 374.181"></polygon>
+                                    <polygon points="330.529 16 297.559 16 178.441 496 211.412 496 330.529 16"></polygon>
+                                </svg>
+                            </div>
+                            <div className="flex flex-col justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
+                                <span className="text-xs tracking-wider uppercase dark:text-violet-400">Step 4</span>
+                                <span className="text-xl font-bold md:text-2xl">Explicabo eaque</span>
+                                <span className="mt-4 dark:text-coolGray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis, voluptates error alias dolorem praesentium sit soluta iure incidunt labore explicabo eaque, quia architecto veritatis dolores, enim cons equatur nihil ipsum.</span>
+                            </div>
                         </div>
-                    </div>
-                    </div>
-                    <div className="flex-1 flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14 2h-7.229l7.014 7h-13.785v6h13.785l-7.014 7h7.229l10-10z"/></svg>
-                    </div>
-                    <div className="w-1/3 text-center px-6">
-                    <div className="bg-gray-300 rounded-lg flex items-center justify-center border border-gray-200">
-                        <div className="w-1/3 bg-transparent h-20 flex items-center justify-center icon-step">
-                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21 6.285l-11.16 12.733-6.84-6.018 1.319-1.49 5.341 4.686 9.865-11.196 1.475 1.285z"/></svg>
-                        </div>
-                        <div className="w-2/3 bg-gray-200 h-24 flex flex-col items-center justify-center px-1 rounded-r-lg body-step">
-                        <h2 className="font-bold text-sm">Confirmation</h2>
-                        <p className="text-xs text-gray-600">
-                            Finish it!
-                        </p>
-                        </div>
-                    </div>
                     </div>
                 </div>
-            </div>
+            </section>
         );
     }
 
@@ -79,9 +90,7 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section>
-                    <RenderSteps />
-                </section>
+                <RenderSteps />
             </div>
         );
     }
